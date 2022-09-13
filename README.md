357c009a6ed06425016b2f9ef0c5cf59b1f2bcda
Willian Nawate <wnoliveira@wno.co.com>
2022-09-13 18:52:23 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # graylog-stack
 Stack do Graylog para docker-compose
+
+Para pode rodar o stack é necessário instalar o docker e docker-compose
+
+```bash
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+apt -qq install -y curl git docker docker-compose nodejs
+npm install elasticdump -g
+```
+
+Para alterar a senha do MongoDB alterar o arquivo: ./secrets/.dbpass
+
+Para alterar a senha do graylog gerar a senha com o comando abaixo em um sistema linux e substituilo no campo GRAYLOG_ROOT_PASSWORD dentro do arquivo: ./env/.env_graylog
+
+```bash
+echo -n "Enter Password: " && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1
+```
+
+No mesmo arquivo também pode ser alterado o nome e e-mail do usuário administrador do sistema.
+Para devido funcinamento do web interface é necessário substituir a tag <ip> pelo endereço IP da máquina host nos campos GRAYLOG_HTTP_PUBLISH_URI e GRAYLOG_HTTP_EXTERNAL_URI.
+
+Para fazer o dump basta substituir a tag <ip> dentro do arquivo logdump.sh e executá-lo com o comando abaixo incluindo o número do indice quando solicitado.
+
+```bash
+bash logdump.sh
+~# bash logdump.sh
+##
+Número do indice para fazer dump [graylog_XXX]: 10
+####
+# Reabrindo indice ...
+####
+...
+...
+...
+...
+```
--- logdump.sh
+++ logdump.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 #
-echo -e "####\n# Reabrindo indices ...\n####"
+while [[ ! $index =~ ^[[:digit:]]+$ ]]; do 
+    echo "##"
+    read -p 'Número do indice para fazer dump [graylog_XXX]: ' index
+	if ! [[ $index =~ ^[[:digit:]]+$ ]]
+		then
+			echo "Insira apenas números!\n"
+	fi
+done
 #
-curl -u admin:testegraylog -H 'Accept: application/json' -H 'X-Requested-by:CLI' -X POST 'http://<ip>:9000/api/system/indexer/indices/graylog_xxx/reopen' & \
+echo -e "####\n# Reabrindo indice ...\n####"
+#
+curl -u admin:testegraylog -H 'Accept: application/json' -H 'X-Requested-by:CLI' -X POST "http://<ip>:9000/api/system/indexer/indices/graylog_$index/reopen"
 #
 sleep 100
 #
-curl admin:testegraylog -H 'Accept: application/json' -H 'X-Requested-by:CLI' -X POST 'http://<ip>:9000/api/system/indices/ranges/graylog_xxx/rebuild' & \
+curl admin:testegraylog -H 'Accept: application/json' -H 'X-Requested-by:CLI' -X POST "http://<ip>:9000/api/system/indices/ranges/graylog_$index/rebuild"
 #
 sleep 40
 #
-echo -e "####\n# Dump de graylog_xxx  ...\n####"
+echo -e "####\n# Dump de graylog_$index ...\n####"
 #
  elasticdump \
-  --input=http://localhost:9200/graylog_xxx/ \
-  --output=/data/log_dump/graylog_xxx.json \
+  --input=http://localhost:9200/graylog_$index/ \
+  --output=/data/log_dump/graylog_$index.json \
   --type=data \
   --limit=10000
 #
 sleep 200
 #
-if [[ -f /data/log_dump/graylog_xxx.json ]]
+if [[ -f /data/log_dump/graylog_$index.json ]]
 then
-	echo -e "####\n# Compactando graylog_xxx ...\n####"
+	echo -e "####\n# Compactando graylog_$index ...\n####"
 #
-	tar -jcvf /data/log_dump/graylog_1463.tgz /data/log_dump/graylog_xxx.json
+	tar -jcvf /data/log_dump/graylog_1463.tgz /data/log_dump/graylog_$index.json
 else
 	echo -e "####\n# Dump imcompleto!\n####"
 	exit
@@ -32,11 +41,11 @@ fi
 #
 sleep 200
 #
-if [[ -f /data/log_dump/graylog_xxx.tgz ]]
+if [[ -f /data/log_dump/graylog_$index.tgz ]]
 then
-	echo -e "####\n# Removendo json de graylog_xxx ...\n####"
+	echo -e "####\n# Removendo json de graylog_$index ...\n####"
 #
-	rm /data/log_dump/graylog_xxx.json
+	rm /data/log_dump/graylog_$index.json
 else
 	echo -e "####\n# Compactação imcompleta!\n####"
 	exit
@@ -44,7 +53,7 @@ fi
 #
 echo -e "####\n# Fechando indices ...\n####"
 #
-curl admin:testegraylog -H 'Accept: application/json' -H 'X-Requested-by:CLI' -X POST 'http://<ip>:9000/api/system/indexer/indices/graylog_xxx/close' & \
+curl admin:testegraylog -H 'Accept: application/json' -H 'X-Requested-by:CLI' -X POST "http://<ip>:9000/api/system/indexer/indices/graylog_$index/close" & \
 #
 echo -e "####\n# Fim!\n####"
 ##
